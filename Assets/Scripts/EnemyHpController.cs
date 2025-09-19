9e9817cc5b0d94e110efdd6552540f7306eb2e6c
kopoia <kopoia0707@gmail.com>
2025-09-19 20:33:41 +0900

    突進したらHP減るように

--- Assets/Scripts/EnemyHpController.cs
+++ Assets/Scripts/EnemyHpController.cs
@@ -69,9 +69,8 @@ public class EnemyHpController : MonoBehaviour
                 yield return new WaitForSeconds(0.01f);
                 transform.Translate(0f, 0f, -0.055f);
             }
-
-            yield return new WaitForSeconds(1.0f);
-
+            yield return new WaitForSeconds(0.5f);
+            playerController.playerCurrentHP -= 30;
             for (int k = 0; k < 100; k++)
             {
                 yield return new WaitForSeconds(0.01f);
